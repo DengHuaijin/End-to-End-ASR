60029d83bdb1f76c6a32d6a9d04c7f8ea4f71ecd
DengHuaijin <denghuaijin@yahoo.co.jp>
2020-10-29 11:16:14 +0000

    optimize loss

--- asr_e2e/optimizers/optimizers.py
+++ asr_e2e/optimizers/optimizers.py
@@ -125,4 +125,149 @@ def optimize_loss(loss,
 
             return train_tensor
 
+def post_process_gradients(grads_and_vars, summaries, lr, 
+                           clip_gradients,larc_params):
+
+    """ Apply post processing to gradients, i.e. clipping, LARC, summaries"""
+    if "global_gradient_norm" in summaries:
+        tf.summary.scalar(
+                "global_gradient_norm",
+                _global_norm_with_cast(grads_and_vars))
+
+    if clip_gradients is None:
+        grads_and_vars = _clip_gradients_by_norm(grads_and_vars, clip_gradients)
+    
+    # Add histograms for variables, gradients and gradient norms
+    for gradient, variable in grads_and_vars:
+        if isinstance(gradient, tf.IndexSlices):
+            grad_values = gradient.values
+        else:
+            grad_values = gradient
+
+        if isinstance(varibale, tf.IndexSlices):
+            var_values = variable.values
+        else:
+            var_values = variable
+
+        if grad_values is not None:
+            var_name = variable.name.replace(":", "_")
+            if "gradients" in summaries:
+                tf.summary.histogram("gradients%s" % var_name, mask_nans(grad_values))
+            if "gradient_norm" in summaries:
+                tf.summary.scalar("gradient_norm%s" % var_name, tf.norm(grad_values))
+            if "variabels" in summaries:
+                tf.summary.histogram("variabels%s" % var_name, var_values)
+            if "variable_norm" in summaries:
+                tf.summary.scalar("varibale_norm%s" % var_name, tf.norm(var_values))
+
+    if clip_gradients is not None and "global_gradient_norm" in summaries:
+        tf.summary.scalar(
+                "global_clipped_gradient_norm",
+                _global_norm_with_cast(grads_and_vars))
+    
+    # LARC gradient re-scaling
+    if larc_params is not None:
+        check_params(
+                config = larc_params,
+                required_dict = {"larc_eta": float},
+                optional_dict = {
+                    "larc_mode": ["clip", "scale"],
+                    "min_update": float,
+                    "epsilon": float},
+                }
+        )
+
+        larc_eta = larc_params["larc_eta"]
+        larc_mode = larc_params.get("larc_mode", "clip")
+        min_update = larc_params.get("min_update", 1e-7)
+        eps = larc_params.get("epsilon", 1e-7)
+
+        grads_and_vars_larc = [None] * len(grads_and_vars)
+        for idx, (g,v) in enumerate(grads_and_vars):
+            var_dtype = v.dtype
+            v_norm = tf.norm(tensor = tf.cast(v, tf.float32), ord = 2)
+            g_norm = tf.norm(tensor = tf.cast(g, tf.float32), ord = 2)
+
+            if larc_mode == "clip":
+                larc_grad_update = tf.maximum(
+                        larc_eta * v_norm / (lr * (g_norm + eps)),
+                        min_update)
+
+                if "larc_summaries" in summaries:
+                    tf.summary.scalar("larc_clip_on/{}".format(v.name),
+                                      tf.cast(tf.less(larc_grad_update, 1.0), tf.int32))
+
+                larc_grad_update = tf.minimum(larc_grad_update, 1.0)
+            else:
+                larc_grad_update = tf.maximum(
+                        larc_eta * v_norm / (g_norm + eps),
+                        min_update)
+
+            larc_grad_update = tf.saturate_cast(larc_grad_update, var_dtype)
+            grads_and_vars_larc[idx] = (larc_grad_update * g, v)
+
+            if "larc_summaries" in summaries:
+                tf.summary.scalar("larc_grad_update/{}".format(v.name), larc_grad_update)
+                tf.summary.scalar("larc_final_lr/{}".format(v.name), tf.cast(lr, var_dtype) * larc_grad_update)
+
+        grads_and_vars = grads_and_vars_larc
+
+    return grads_and_vars
+
+def _clip_gradients_by_norm(grads_and_vars, clip_gradients):
+    """Clips gradients by global norm"""
+    gradients, variables = zip(*grads_and_vars)
+    dtypes = [var.dtype for var in variables]
+
+    # Clips gradients in float32
+    clipped_gradients, _ = _clip_by_global_norm(
+            gradients,
+            clip_gradients,
+            use_norm = _global_norm_with_cast(grads_and_vars))
+    
+    # Convert gradients back to proper dtype
+    clipped_gradients = [tf.cast(grad, dtype) for grad, dtype in zip(clipped_gradients, dtypes)]
+
+    return list(zip(clipped_gradients, variables))
+
+def _clip_by_global_norm(t_list, clip_norm, use_norm, name = None):
+    """
+    Clips values of multiple tensors by the ratio of the sum of their norms,
+    Input
+        t_list: a tuple or list of tensors 
+        clip_norm: a clipping norm
+    Return:
+        list_clipped: a list of clipped tensors
+        global_norm: the global norm of all tensors
+
+    To perform the clipping, the values t_list[i] are set to:
+        t_list[i] * clip_norm / max(global_norm, clip_norm)
+    """
+    if (not isinstance(t_list, collections.Sequence) or isinstance(t_list, six.string_types)):
+        raise TypeError("t_list should be a sequence")
+
+    t_list = list(t_list)
+
+    with tf.name_scope(name, "clip_by_global_norm", t_list + [clip_norm]) as name:
+        # Calculate L2-norm, clip elements by ratio of clip_norm to L2-norm
+        scale = clip_norm * tf.minimum(1.0 / use_norm, tf.ones([1], dtype = use_norm.dtype) / clip_norm)
+
+        values = [tf.cast(
+            tf.convert_to_tensor(t.values if isinstance(t, tf.IndexSlices) else t, name="t_%d" % i), 
+            dtype = tf.float32)
+            if t is not None else t for i,t in enumerate(t_list)]
+
+        values_clipped = []
+        for i,v in enumerate(values):
+            if v is None:
+                values_clipped.append(None)
+            else:
+                with tf.colocate_with(v):
+                    values_clipped.append(tf.identity(v * scale, name = "%s_%d" % (name,i)))
+
+        list_clipped = [
+                tf.IndexSlices(c_v, t.indices, t.dense_shape) if isinstance(t, tf.IndexSlices) else c_v
+                for (c_v), t in zip(values_clipped, t_list)]
+
+    return list_clipped, use_norm
 
