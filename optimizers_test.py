949a45b4f252a92d907f12859064f00248644e21
DengHuaijin <denghuaijin@yahoo.co.jp>
2020-11-20 13:32:34 +0000

    optimizer test pass

--- optimizers_test.py
+++ optimizers_test.py
@@ -9,7 +9,7 @@ from six.moves import range
 from asr_e2e.optimizers.optimizers import optimize_loss
 from asr_e2e.optimizers.lr_policies import fixed_lr
 
-class IterSizeTests(tf.test.TestCase):
+class OptimizerTests(tf.test.TestCase):
 
     def SetUp(self):
         pass
@@ -30,9 +30,9 @@ class IterSizeTests(tf.test.TestCase):
 
             X = np.random.rand(n_samples, n_hid)
             y = np.random.rand(n_samples, 1)
-            # w == np.dot(np.linalg.inv(X), y)
-            w = np.linalg.solve(X.T.dot(X), X.T.dot(y))
-
+            w = np.dot(np.linalg.inv(X), y)
+            # w = np.linalg.solve(X.T.dot(X), X.T.dot(y))
+            
             x_ph = tf.placeholder(var_dtype, [n_samples, n_hid])
             y_ph = tf.placeholder(var_dtype, [n_samples, 1])
 
@@ -41,20 +41,26 @@ class IterSizeTests(tf.test.TestCase):
             loss += tf.losses.get_regularization_loss()
             skip_update_ph = tf.placeholder(tf.bool)
             # iter_size = 8
-            train_op = optimize_loss(loss, "SGD", {},
-                                     lambda gs: fixed_lr(gs, 0.1), dtype = dtype,
+            train_op, grads_and_vars = optimize_loss(loss, "SGD", {},
+                                     lambda gs: fixed_lr(gs, 0.3), dtype = dtype,
                                      skip_update_ph = skip_update_ph)
             
             for var in tf.global_variables():
                 print(var.name)
             
-            # grad_accum = None
             var = tf.trainable_variables()[0]
+            grad, _ = grads_and_vars[0]
             with self.test_session(g, use_gpu = True) as sess:
                 
                 sess.run(tf.global_variables_initializer())
-                
-                for _ in range(3):
+                print("Before update: {}".format(sess.run(var)))
+                # print("y label: {}".format(y))
+                for i in range(5):
+                    _, v_value, new_grad, loss_value, y_value = sess.run([train_op, var, grad, loss, y_pred],  {x_ph: X, y_ph: y})
+                    
+                    true_grad = 2 * (X.T.dot(X).dot(v_value) - X.T.dot(y)) / X.shape[0]
+                    npt.assert_allclose(new_grad, true_grad, atol = 1e-7)
+                    """
                     v = sess.run([var])
                     # npt.assert_allclose(g, np.zeros(g.shape))
 
@@ -79,6 +85,12 @@ class IterSizeTests(tf.test.TestCase):
                     v_new = sess.run([var])
                     # npt.assert_allclose(g_new, np.zeros(g.shape))
                     npt.assert_allclose(v_new, v - 0.1 * true_g * 4, atol = 1e-7)
-
+                    """
+                    print("epoch {} var: {}  Loss: {}".format(i, sess.run([var]), loss_value))
+                    # print("y_pred: {}".format(y_value))
+                print(w)
+                print("y label: {}".format(y))
+                print("y_pred: {}".format(y_value))
+                print(np.mean((y_value - y)**2))
 tf.test.main()
 
